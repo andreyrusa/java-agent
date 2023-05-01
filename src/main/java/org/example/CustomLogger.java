7dc7b38b9a2265c2eb555e58d1a0ea87a020c769
Andrey Ruiz <andreyrusa@gmail.com>
2023-05-01 17:10:52 +0200

    Delivered version

--- src/main/java/org/example/CustomLogger.java
+++ src/main/java/org/example/CustomLogger.java
@@ -1,383 +1,83 @@
 package org.example;
 
-import org.slf4j.Logger;
-import org.slf4j.Marker;
-import org.slf4j.helpers.MarkerIgnoringBase;
-
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.HashSet;
+import java.util.Set;
 
-public class CustomLogger extends MarkerIgnoringBase implements Logger {
-    private final Logger delegate;
+public class CustomLogger implements InvocationHandler {
+    private final Object originalLogger;
     private static final String LOG_FILE_PATH = "agent-logs.txt";
-    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+    private static final Set<String> uniqueLogMessages = new HashSet();
 
-    private void writeToFile(String message) {
-        try (FileWriter fw = new FileWriter(LOG_FILE_PATH, true);
-             PrintWriter pw = new PrintWriter(fw)) {
-            String timestamp = DATE_FORMAT.format(new Date());
-            pw.println(timestamp + " - " + message);
-        } catch (IOException e) {
-            e.printStackTrace();
+    private CustomLogger(Object originalLogger) {
+        this.originalLogger = originalLogger;
+    }
+
+    public static Object wrapLogger(Object logger) {
+        return Proxy.newProxyInstance(logger.getClass().getClassLoader(), logger.getClass().getInterfaces(), new CustomLogger(logger));
+    }
+
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        if (("info".equals(method.getName()) || "debug".equals(method.getName()) || "warn".equals(method.getName()) || "error".equals(method.getName())) && args != null && args.length > 0) {
+            String logMessage = String.valueOf(args[0]);
+            Object[] logArgs = args.length > 1 ? (Object[])((Object[])args[1]) : null;
+            Method getLoggerNameMethod = this.originalLogger.getClass().getMethod("getName");
+            String loggerName = (String)getLoggerNameMethod.invoke(this.originalLogger);
+            Instant currentTime = Instant.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS").withZone(ZoneId.systemDefault());
+            String timestamp = formatter.format(currentTime);
+            String threadName = Thread.currentThread().getName();
+            String formattedLogMessage;
+            if (logArgs != null) {
+                formattedLogMessage = String.format(logMessage, logArgs);
+            } else {
+                formattedLogMessage = logMessage;
+            }
+
+            formattedLogMessage = String.format("%s [%s] %s %s - %s", timestamp, threadName, method.getName().toUpperCase(), loggerName, formattedLogMessage);
+            if (uniqueLogMessages.add(formattedLogMessage)) {
+                this.writeLogToFile(formattedLogMessage);
+            }
         }
-    }
-    public CustomLogger(Logger delegate) {
-        this.delegate = delegate;
-    }
 
-    @Override
-    public String getName() {
-        return delegate.getName();
+        return method.invoke(this.originalLogger, args);
     }
 
-    @Override
-    public boolean isTraceEnabled() {
-        return delegate.isTraceEnabled();
-    }
+    private void writeLogToFile(String logMessage) {
+        try {
+            FileWriter fileWriter = new FileWriter("agent-logs.txt", true);
+            Throwable var3 = null;
 
-    @Override
-    public void trace(String msg) {
-        delegate.trace(msg);
-        writeToFile(msg);
-    }
+            try {
+                fileWriter.write(logMessage + System.lineSeparator());
+            } catch (Throwable var13) {
+                var3 = var13;
+                throw var13;
+            } finally {
+                if (fileWriter != null) {
+                    if (var3 != null) {
+                        try {
+                            fileWriter.close();
+                        } catch (Throwable var12) {
+                            var3.addSuppressed(var12);
+                        }
+                    } else {
+                        fileWriter.close();
+                    }
+                }
 
-    @Override
-    public void trace(String s, Object o) {
-        delegate.trace(s);
-        writeToFile(s);
-    }
+            }
+        } catch (IOException var15) {
+            System.err.println("Error writing log to file: " + var15.getMessage());
+        }
 
-    @Override
-    public void trace(String s, Object o, Object o1) {
-        delegate.trace(s);
-        writeToFile(s);
-    }
-
-    @Override
-    public void trace(String s, Object... objects) {
-        delegate.trace(s);
-        writeToFile(s);
-    }
-
-    @Override
-    public void trace(String s, Throwable throwable) {
-        delegate.trace(s);
-        writeToFile(s);
-    }
-
-    @Override
-    public boolean isTraceEnabled(Marker marker) {
-        return delegate.isTraceEnabled();
-    }
-
-    @Override
-    public void trace(Marker marker, String s) {
-        delegate.trace(s);
-        writeToFile(s);
-    }
-
-    @Override
-    public void trace(Marker marker, String s, Object o) {
-        delegate.trace(s);
-        writeToFile(s);
-    }
-
-    @Override
-    public void trace(Marker marker, String s, Object o, Object o1) {
-        delegate.trace(s);
-        writeToFile(s);
-    }
-
-    @Override
-    public void trace(Marker marker, String s, Object... objects) {
-        delegate.trace(s);
-        writeToFile(s);
-    }
-
-    @Override
-    public void trace(Marker marker, String s, Throwable throwable) {
-        delegate.trace(s);
-        writeToFile(s);
-    }
-
-    @Override
-    public boolean isDebugEnabled() {
-        return delegate.isDebugEnabled();
-    }
-
-    @Override
-    public void debug(String s) {
-        delegate.debug(s);
-        writeToFile(s);
-    }
-
-    @Override
-    public void debug(String s, Object o) {
-        delegate.debug(s);
-        writeToFile(s);
-    }
-
-    @Override
-    public void debug(String s, Object o, Object o1) {
-        delegate.debug(s);
-        writeToFile(s);
-    }
-
-    @Override
-    public void debug(String s, Object... objects) {
-        delegate.debug(s);
-        writeToFile(s);
-    }
-
-    @Override
-    public void debug(String s, Throwable throwable) {
-        delegate.debug(s);
-        writeToFile(s);
-    }
-
-    @Override
-    public boolean isDebugEnabled(Marker marker) {
-        return delegate.isDebugEnabled();
-    }
-
-    @Override
-    public void debug(Marker marker, String s) {
-        delegate.debug(s);
-        writeToFile(s);
-    }
-
-    @Override
-    public void debug(Marker marker, String s, Object o) {
-        delegate.debug(s);
-        writeToFile(s);
-    }
-
-    @Override
-    public void debug(Marker marker, String s, Object o, Object o1) {
-        delegate.debug(s);
-        writeToFile(s);
-    }
-
-    @Override
-    public void debug(Marker marker, String s, Object... objects) {
-        delegate.debug(s);
-        writeToFile(s);
-    }
-
-    @Override
-    public void debug(Marker marker, String s, Throwable throwable) {
-        delegate.debug(s);
-        writeToFile(s);
-    }
-
-    @Override
-    public boolean isInfoEnabled() {
-        return delegate.isInfoEnabled();
-    }
-
-    @Override
-    public void info(String s) {
-        delegate.info(s);
-        writeToFile(s);
-    }
-
-    @Override
-    public void info(String s, Object o) {
-        delegate.info(s);
-        writeToFile(s);
-    }
-
-    @Override
-    public void info(String s, Object o, Object o1) {
-        delegate.info(s);
-        writeToFile(s);
-    }
-
-    @Override
-    public void info(String s, Object... objects) {
-        delegate.info(s);
-        writeToFile(s);
-    }
-
-    @Override
-    public void info(String s, Throwable throwable) {
-        delegate.info(s);
-        writeToFile(s);
-    }
-
-    @Override
-    public boolean isInfoEnabled(Marker marker) {
-        return delegate.isInfoEnabled();
-    }
-
-    @Override
-    public void info(Marker marker, String s) {
-        delegate.info(s);
-        writeToFile(s);
-    }
-
-    @Override
-    public void info(Marker marker, String s, Object o) {
-        delegate.info(s);
-        writeToFile(s);
-    }
-
-    @Override
-    public void info(Marker marker, String s, Object o, Object o1) {
-        delegate.info(s);
-        writeToFile(s);
-    }
-
-    @Override
-    public void info(Marker marker, String s, Object... objects) {
-        delegate.info(s);
-        writeToFile(s);
-    }
-
-    @Override
-    public void info(Marker marker, String s, Throwable throwable) {
-        delegate.info(s);
-        writeToFile(s);
-    }
-
-    @Override
-    public boolean isWarnEnabled() {
-        return delegate.isWarnEnabled();
-    }
-
-    @Override
-    public void warn(String s) {
-        delegate.warn(s);
-        writeToFile(s);
-    }
-
-    @Override
-    public void warn(String s, Object o) {
-        delegate.warn(s);
-        writeToFile(s);
-    }
-
-    @Override
-    public void warn(String s, Object... objects) {
-        delegate.warn(s);
-        writeToFile(s);
-    }
-
-    @Override
-    public void warn(String s, Object o, Object o1) {
-        delegate.warn(s);
-        writeToFile(s);
-    }
-
-    @Override
-    public void warn(String s, Throwable throwable) {
-        delegate.warn(s);
-        writeToFile(s);
-    }
-
-    @Override
-    public boolean isWarnEnabled(Marker marker) {
-        return delegate.isWarnEnabled();
-    }
-
-    @Override
-    public void warn(Marker marker, String s) {
-        delegate.warn(s);
-        writeToFile(s);
-    }
-
-    @Override
-    public void warn(Marker marker, String s, Object o) {
-        delegate.warn(s);
-        writeToFile(s);
-    }
-
-    @Override
-    public void warn(Marker marker, String s, Object o, Object o1) {
-        delegate.warn(s);
-        writeToFile(s);
-    }
-
-    @Override
-    public void warn(Marker marker, String s, Object... objects) {
-        delegate.warn(s);
-        writeToFile(s);
-    }
-
-    @Override
-    public void warn(Marker marker, String s, Throwable throwable) {
-
-    }
-
-    @Override
-    public boolean isErrorEnabled() {
-        return delegate.isErrorEnabled();
-    }
-
-    @Override
-    public void error(String s) {
-        delegate.error(s);
-        writeToFile(s);
-    }
-
-    @Override
-    public void error(String s, Object o) {
-        delegate.error(s);
-        writeToFile(s);
-    }
-
-    @Override
-    public void error(String s, Object o, Object o1) {
-        delegate.error(s);
-        writeToFile(s);
-    }
-
-    @Override
-    public void error(String s, Object... objects) {
-        delegate.error(s);
-        writeToFile(s);
-    }
-
-    @Override
-    public void error(String s, Throwable throwable) {
-
-    }
-
-    @Override
-    public boolean isErrorEnabled(Marker marker) {
-        return delegate.isErrorEnabled();
-    }
-
-    @Override
-    public void error(Marker marker, String s) {
-        delegate.error(s);
-        writeToFile(s);
-    }
-
-    @Override
-    public void error(Marker marker, String s, Object o) {
-        delegate.error(s);
-        writeToFile(s);
-    }
-
-    @Override
-    public void error(Marker marker, String s, Object o, Object o1) {
-        delegate.error(s);
-        writeToFile(s);
-    }
-
-    @Override
-    public void error(Marker marker, String s, Object... objects) {
-        delegate.error(s);
-        writeToFile(s);
-    }
-
-    @Override
-    public void error(Marker marker, String s, Throwable throwable) {
-        delegate.error(s);
-        writeToFile(s);
     }
 }
+
--- src/main/java/org/example/LogCaptureAgent.java
+++ src/main/java/org/example/LogCaptureAgent.java
@@ -1,15 +1,17 @@
 package org.example;
 
+import java.lang.instrument.Instrumentation;
 import net.bytebuddy.agent.builder.AgentBuilder;
 import net.bytebuddy.asm.Advice;
-import net.bytebuddy.implementation.bytecode.assign.Assigner;
-import org.slf4j.Logger;
-
-import java.lang.instrument.Instrumentation;
-
-import static net.bytebuddy.matcher.ElementMatchers.named;
+import net.bytebuddy.asm.Advice.OnMethodExit;
+import net.bytebuddy.asm.Advice.Return;
+import net.bytebuddy.implementation.bytecode.assign.Assigner.Typing;
+import net.bytebuddy.matcher.ElementMatchers;
 
 public class LogCaptureAgent {
+    public LogCaptureAgent() {
+    }
+
     public static void premain(String agentArgs, Instrumentation inst) {
         configureAgent(inst);
     }
@@ -19,18 +21,18 @@ public class LogCaptureAgent {
     }
 
     private static void configureAgent(Instrumentation inst) {
-        new AgentBuilder.Default()
-                .type(named("org.slf4j.LoggerFactory"))
-                .transform((builder, typeDescription, classLoader, module) -> builder
-                        .method(named("getLogger"))
-                        .intercept(Advice.to(GetLoggerAdvice.class))
-                ).installOn(inst);
+        (new AgentBuilder.Default()).type(ElementMatchers.named("org.slf4j.LoggerFactory")).transform((builder, typeDescription, classLoader, module) -> {
+            return builder.method(ElementMatchers.named("getLogger")).intercept(Advice.to(GetLoggerAdvice.class));
+        }).installOn(inst);
     }
 
     public static class GetLoggerAdvice {
-        @Advice.OnMethodExit
-        public static void exit(@Advice.Return(readOnly = false, typing = Assigner.Typing.DYNAMIC) Logger logger) {
-            logger = new CustomLogger(logger);
+        public GetLoggerAdvice() {
+        }
+
+        @OnMethodExit
+        public static void exit(@Return(readOnly = false,typing = Typing.DYNAMIC) Object logger) {
+            logger = CustomLogger.wrapLogger(logger);
         }
     }
 }
